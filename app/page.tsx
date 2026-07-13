8d951b0cb109d8d03e68768f3b5af26f1aff76af
Naggy Tsuda <23202@apps.eggs.school.nz>
2026-07-13 15:16:41 +1200

    remove initial features

--- app/page.tsx
+++ app/page.tsx
@@ -12,23 +12,24 @@ export default async function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
+        {/* <Image
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
           width={100}
           height={20}
           priority
-        />
+        /> */}
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <hr />
-          yhhyhyhy
+          {/* yhhyhyhy */}
 
-          <div>{staffList?.map(staff => (
+          < div > {staffList?.map(staff => (
             <div
               key={staff.id}/*  */
             >{staff.name}</div>
-          ))}</div>
+          ))
+          }</div >
           <div>          ========================        </div>
 
           <div>
@@ -36,17 +37,17 @@ export default async function Home() {
           </div>
           <div>          ========================        </div>
 
-          iuijiujiujuijijiu
-          <hr />
-          <Button variant="contained" color="error" size="large">Button</Button>
-          <Alert icon={<VerifiedIcon fontSize="inherit" />} severity="warning">
+          {/* iuijiujiujuijijiu */}
+          < hr />
+          {/* <Button variant="contained" color="error" size="large">Button</Button> */}
+          {/* <Alert icon={<VerifiedIcon fontSize="inherit" />} severity="warning">
             Here is a gentle confirmation that your action was successful.
-          </Alert>
-          <Typography variant="h1">Hello</Typography>
+          </Alert> */}
+          {/* <Typography variant="h1">Hello</Typography>
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             Hello world 4
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+          </h1> */}
+          {/* <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -62,10 +63,10 @@ export default async function Home() {
               Learning
             </a>{" "}
             center.
-          </p>
-        </div>
+          </p> */}
+        </div >
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
+          {/* <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
@@ -79,17 +80,17 @@ export default async function Home() {
               height={16}
             />
             Deploy Now
-          </a>
-          <a
+          </a> */}
+          {/* <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
             Documentation
-          </a>
+          </a> */}
         </div>
-      </main>
-    </div>
+      </main >
+    </div >
   );
 }
