2f76222167ddbbf8f667566c9728c15e33b6dd5e
Naggy Tsuda <23202@apps.eggs.school.nz>
2026-08-09 14:51:11 +1200

    used mui text field

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useActionState } from "react";
-import { Button } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
 import { login, signup } from "./actions";
 
 const initialState: string | null = null;
@@ -17,20 +17,36 @@ export default function LoginPage() {
   );
 
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <br />
+    <>
+      <h1 style={{ textAlign: "center", fontSize: "32px" }}>
+        Login Form
+      </h1>
       {loginState && <p style={{ color: 'red' }}>{loginState}</p>}
       {signupState && <p style={{ color: 'red' }}>{signupState}</p>}
-      <Button formAction={loginAction} disabled={loginPending}>
-        Log in
-      </Button>
-      <Button formAction={signupAction} disabled={signupPending}>
-        Sign up
-      </Button>
-    </form>
+
+      <Box
+        component="form"
+        sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: 300, margin: 'auto', mt: 4 }}
+      >
+        <TextField
+          label="Email"
+          name="email" // Required for Server Actions to read the value
+          type="email"
+          required
+        />
+        <TextField
+          label="Password"
+          name="password" // Required for Server Actions to read the value
+          type="password"
+          required
+        /><br />
+        <Button formAction={loginAction} type="submit" variant="contained" disabled={loginPending}>
+          Log in
+        </Button>
+        <Button formAction={signupAction} type="submit" variant="outlined" disabled={signupPending}>
+          Sign up
+        </Button>
+      </Box>
+    </>
   );
 }
